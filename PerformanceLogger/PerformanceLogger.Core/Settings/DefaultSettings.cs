0bbe280683e279de5de0daeeb82af97589cebe0c
Alex.Slabukh <aslabukh@gmail.com>
2021-09-06 22:54:58 +0700

    PerformanceLogger.Core: Add default settings

---
+++ PerformanceLogger/PerformanceLogger.Core/Settings/DefaultSettings.cs
@@ -0,0 +1,12 @@
+﻿using PerformanceLogger.Core.EnumTypes;
+
+namespace PerformanceLogger.Core.Settings
+{
+    internal class DefaultSettings : ILoggerSettings
+    {
+        public LogLevel LogLevel => LogLevel.Info;
+        public OperationsType Operation => OperationsType.None;
+        public ObjectType ObjectType => ObjectType.None;
+        public ErrorType ErrorType => ErrorType.None;
+    }
+}
---
+++ PerformanceLogger/PerformanceLogger.Core/Settings/ILoggerSettings.cs
@@ -0,0 +1,12 @@
+﻿using PerformanceLogger.Core.EnumTypes;
+
+namespace PerformanceLogger.Core.Settings
+{
+    internal interface ILoggerSettings
+    {
+        LogLevel LogLevel { get; }
+        OperationsType Operation { get; }
+        ObjectType ObjectType { get; }
+        ErrorType ErrorType { get; }
+    }
+}
